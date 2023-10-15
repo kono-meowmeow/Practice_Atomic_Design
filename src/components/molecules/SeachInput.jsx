6fcf8f202c25de9f5d1f2be37d47d10f04bbed2e
kono-meowmeow <aya.ayatsuki@gmail.com>
2023-10-15 23:33:32 +0900

    styleの適用

--- src/components/molecules/SeachInput.jsx
+++ src/components/molecules/SeachInput.jsx
@@ -1,12 +1,27 @@
+import styled from "styled-components";
+
 import { PrimaryButton } from "../atoms/button/PrimaryButton"
 import { Input } from "../atoms/input/Input"
 
 // SeachInputというコンポーネントをimportすることで、どこでも検索ボックスが使えるようになる
 export const SearchInput = () => {
   return (
-    <div>
+    <SContainer>
       <Input placeholder="検索条件を入力" />
-      <PrimaryButton>検索</PrimaryButton>
-    </div>
+      <SButtonWrapper>
+        <PrimaryButton>検索</PrimaryButton>
+      </SButtonWrapper>
+    </SContainer>
   );
 };
+
+// divタグ内の要素を横並びにする
+const SContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+// 検索ボタンの左側に余白を設ける
+const SButtonWrapper = styled.div`
+  padding-left: 8px;
+`;

82bbe9a5f972be7de3889329e3ad6102095d63c0
kono-meowmeow <aya.ayatsuki@gmail.com>
2023-10-17 23:29:06 +0900

    pageごとにlayoutを適用する

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,14 +1,31 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Top } from "../components/pages/Top"
 import { Users } from "../components/pages/Users"
+import { DefaultLayout } from "../components/templates/DefaultLayout";
+import { HeaderOnly } from "../components/templates/HeaderOnly";
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         {/* ここにルーティングの設定を書いていく */}
-        <Route path="/" element={<Top />} />
-        <Route path="/users" element={<Users />} />
+        <Route
+          path="/"
+          element={
+            // ここに表示したいコンポーネントを書く
+            <DefaultLayout>
+              <Top />
+            </DefaultLayout>
+          }
+        />
+        <Route
+          path="/users"
+          element={
+            <HeaderOnly>
+              <Users />
+            </HeaderOnly>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
